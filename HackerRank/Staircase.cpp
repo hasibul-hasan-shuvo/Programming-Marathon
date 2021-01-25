779f759e9059b72117b29a6720ca7f0e614b10de
MD Hasibul Hasan Shuvo <shovocse753@gmail.com>
2021-01-25 22:21:19 +0600

    LightOJ-1000 and HackerRank Staircase is added.

---
+++ HackerRank/Staircase.cpp
@@ -0,0 +1,8 @@
+/** Problem link: https://www.hackerrank.com/challenges/staircase/problem **/
+
+#include<cstdio>
+
+int main()
+{
+	return 0;
+}
---
+++ LightOJ/LOJ-1000.cpp
@@ -0,0 +1,8 @@
+/** Problem link: https://lightoj.com/problem/greetings-from-lightoj **/
+
+#include<cstdio>
+
+int main()
+{
+	return 0;
+}
