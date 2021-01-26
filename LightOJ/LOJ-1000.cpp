1453423837a5d4c53171c497e69ce4b5f77cd8a7
Raihan-Chowdhury <RaihanChowdhury.next@gmail.com>
2021-01-26 12:04:13 +0600

    Submit LOJ01 HR01

--- LightOJ/LOJ-1000.cpp
+++ LightOJ/LOJ-1000.cpp
@@ -1,8 +1,12 @@
 /** Problem link: https://lightoj.com/problem/greetings-from-lightoj **/
 
-#include<cstdio>
-
-int main()
-{
-	return 0;
+#include <iostream>
+int main(){
+    int n;
+	std::cin >> n;
+    for(int x=0;x<n;x++){
+        int t1,t2;
+        std::cin >> t1 >> t2;
+        std::cout << "Case " << x+1 << ":" << " " <<t1+t2 << "\n";
+    }
 }
