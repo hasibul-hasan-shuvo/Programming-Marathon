04e8cc7909d06e43499ab0745a8d0ffcc947ebfc
Showvike Mondal Ovi <showvike@gmail.com>
2021-01-27 18:33:26 +0600

    week_01

--- HackerRank/Staircase.cpp
+++ HackerRank/Staircase.cpp
@@ -1,8 +1,27 @@
 /** Problem link: https://www.hackerrank.com/challenges/staircase/problem **/
 
-#include<cstdio>
+#include <cstdio>
+
+void staircase(int n)
+{
+	for(int I = 0; I < n; I++)
+	{
+		for(int K = I; K < n - 1; K++)
+			printf(" ");
+
+		for(int K = 0; K <= I; K++)
+			printf("#");
+
+		printf("\n");
+	}
+}
 
 int main()
 {
+	int n;
+
+	while(scanf("%d", &n) == 1)
+		staircase(n);
+
 	return 0;
 }
--- LightOJ/LOJ-1000.cpp
+++ LightOJ/LOJ-1000.cpp
@@ -1,8 +1,28 @@
 /** Problem link: https://lightoj.com/problem/greetings-from-lightoj **/
 
-#include<cstdio>
+#include <cstdio>
 
 int main()
 {
+	int t;
+
+	while(scanf("%d", &t) == 1)
+	{
+		int I = 0;
+
+		while(t--)
+		{
+			int a, b;
+
+			scanf("%d %d", &a, &b);
+
+			int c = a + b;
+
+			printf("Case %d: %d\n", I + 1, c);
+
+			I++;
+		}
+	}
+
 	return 0;
 }
