a70fc1d191ea72bb70446db94101d11945c459e1
Monowar Hossain Khan <mhkha2022@gmail.com>
2021-01-28 21:55:07 +0600

    Solved HackerRank Staircase

--- HackerRank/Staircase.cpp
+++ HackerRank/Staircase.cpp
@@ -2,7 +2,26 @@
 
 #include<cstdio>
 
+void sazol();
+
 int main()
 {
-	return 0;
+    sazol();
+    return 0;
 }
+
+void sazol() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n - 1 - i; ++j) {
+            printf(" ");
+        }
+        for(int j = 0; j <= i; ++j) {
+            printf("#");
+        }
+        puts("");
+    }
+}
+
