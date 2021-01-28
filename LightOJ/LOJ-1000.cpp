1ceb5f21a67b202b8cc2444174eed7c1eae7eddc
amJayem <csejayem19@gmail.com>
2021-01-29 01:18:50 +0600

    'Greetings from LightOJ LOJ-1000' by jayem.

--- LightOJ/LOJ-1000.cpp
+++ LightOJ/LOJ-1000.cpp
@@ -2,7 +2,22 @@
 
 #include<cstdio>
 
+void jayem();
 int main()
 {
+    	jayem();
 	return 0;
+}
+
+void jayem()
+{
+    int tcase,a,b,i;
+
+    scanf("%d",&tcase);
+
+    for(i=1;i<=tcase;i++)
+    {
+        scanf("%d%d",&a,&b);
+        printf("Case %d: %d\n",i,a+b);
+    }
 }
