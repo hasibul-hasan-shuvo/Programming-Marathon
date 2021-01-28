44920c670534209d9cc5bdafde908161d27bbf1a
Raihan Chowdhury <62661424+Raihan-Chowdhury@users.noreply.github.com>
2021-01-28 23:25:22 +0600

    HackerRank-Staircase

--- HackerRank/Staircase.cpp
+++ HackerRank/Staircase.cpp
@@ -1,8 +1,20 @@
 /** Problem link: https://www.hackerrank.com/challenges/staircase/problem **/
 
 #include<cstdio>
-
+void raihan();
 int main()
 {
+	raihan();
 	return 0;
+}
+void raihan(){
+	int n;
+	scanf("%d",&n);
+	for(int x=1;x<=n;x++){
+		for(int y=1;y<=n;y++){
+			if( y<=n-x )printf(" ");
+			else printf("#");
+		}
+		printf("\n");
+	}
 }
