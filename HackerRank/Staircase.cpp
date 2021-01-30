5fab363e3bbcb99f91a36775ce311a6eb6bfecd1
Tanji Raj <tanjiraj0007@gmail.com>
2021-01-30 16:13:22 +0600

    Week 1

--- HackerRank/Staircase.cpp
+++ HackerRank/Staircase.cpp
@@ -1,8 +1,22 @@
 /** Problem link: https://www.hackerrank.com/challenges/staircase/problem **/
 
-#include<cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+void raj(){
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		for(int j=n-1;j>=0;j--){
+			if(i>=j)cout<<"#";
+			else cout<<" ";
+		}
+		cout<<endl;
+	}
+}
 
 int main()
 {
+	raj();
 	return 0;
 }
--- LightOJ/LOJ-1000.cpp
+++ LightOJ/LOJ-1000.cpp
@@ -1,8 +1,20 @@
 /** Problem link: https://lightoj.com/problem/greetings-from-lightoj **/
 
-#include<cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+void raj(){
+	int t;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+    int a,b;
+    cin>>a>>b;
+    cout<<"Case "<<i<<": "<<a+b<<endl;
+	}
+}
 
 int main()
 {
+	raj();
 	return 0;
 }
