74fd7bfa14d39ab62e1751c1d99e3ea62d769d13
theove46 <57717227+theove46@users.noreply.github.com>
2021-01-31 23:15:07 +0600

    Update LOJ-1000.cpp

--- LightOJ/LOJ-1000.cpp
+++ LightOJ/LOJ-1000.cpp
@@ -1,8 +1,21 @@
 /** Problem link: https://lightoj.com/problem/greetings-from-lightoj **/
 
-#include<cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+void ove()
+{
+    int t, a, b, i;
+    scanf("%d", &t);
+    for(i=1; i<=t; i++)
+    {
+        scanf("%d%d", &a, &b);
+        printf("Case %lld: %lld\n", i, a+b);
+    }
+}
 
 int main()
 {
-	return 0;
+    ove();
+    return 0;
 }
