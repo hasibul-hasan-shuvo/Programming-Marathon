c59b86ceb8aee079930816f18e9b8268fb6f2aca
Showvike Mondal Ovi <showvike@gmail.com>
2021-02-05 19:05:59 +0600

    week_02

--- Codeforces/4A Watermelon.cpp
+++ Codeforces/4A Watermelon.cpp
@@ -1,8 +1,26 @@
 /** Problem link: https://codeforces.com/problemset/problem/4/A **/
 
-#include<cstdio.h>
+#include <cstdio>
+
+void showvike();
 
 int main()
 {
+	showvike();
+
 	return 0;
+}
+
+void showvike()
+{
+	int w;
+
+	while(scanf("%d", &w) == 1)
+	{
+		if((w % 2 == 0) && (w != 2))
+			printf("YES\n");
+
+		else
+			printf("NO\n");
+	}
 }
--- Uva/Uva 100.cpp
+++ Uva/Uva 100.cpp
@@ -1,8 +1,52 @@
 /** Problem link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=36 **/
 
-#include<cstdio.h>
+#include <cstdio>
+
+void showvike();
 
 int main()
 {
+	showvike();
+
 	return 0;
+}
+
+void showvike()
+{
+	int I, J;
+
+	while(scanf("%d %d", &I, &J) == 2)
+	{
+		printf("%d %d", I, J);
+
+		if(I > J)
+		{
+			int t = I;
+			I = J;
+			J = t;
+		}
+
+		int c = 0;
+
+		for(int K = I; K <= J; K++)
+		{
+			int cc = 1, n = K;
+
+			while(n != 1)
+			{
+				cc++;
+
+				if(n % 2 != 0)
+					n = (3 * n) + 1;
+
+				else
+					n = n / 2;
+			}
+
+			if(cc > c)
+				c = cc;
+		}
+
+		printf(" %d\n", c);
+	}
 }
