c1f5890c0684d3250c2e4d4301abd61a15c8bfe1
Monowar Hossain Khan <mhkha2022@gmail.com>
2021-02-27 13:09:47 +0600

    Solved Codeforces 4A

--- Codeforces/4A Watermelon.cpp
+++ Codeforces/4A Watermelon.cpp
@@ -1,8 +1,19 @@
 /** Problem link: https://codeforces.com/problemset/problem/4/A **/
 
-#include<cstdio.h>
+#include<cstdio>
+
+void sazol();
 
 int main()
 {
+  sazol();
 	return 0;
+}
+
+void sazol() {
+  int w;
+  scanf("%d", &w);
+ 
+  if(w <= 2 || w & 1) puts("NO");
+  else puts("YES");
 }
